d6c7b646d8b0d163a2f5761b0deb412a68473f88
Mohammad Aslani <mohammadaslani3366@gmail.com>
2025-05-18 13:59:10 +0330

    eighteenth commit

--- src/components/body/Content.jsx
+++ src/components/body/Content.jsx
@@ -4,7 +4,7 @@ const Content = () => {
   return (
     <div>
 
-      <div className="max-w-5xl mx-auto p-4 pt-0  rounded-2xl shadow-2xl border-[#F2F2F2] border-r-6  flex justify-center items-center  flex-col bg-[#000000]">
+      <div className="max-w-5xl mx-auto  pt-0  rounded-2xl shadow-2xl border-[#F2F2F2] border-r-6  flex justify-center items-center  flex-col ">
       <Resume/>
       </div>
       
